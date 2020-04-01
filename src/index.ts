5a8729141356c18baac1bfd00b9c8d89038ea51b
contesini <allancontesini@hotmail.com>
2020-04-01 15:17:38 -0300

    Adicionado playground e voyager
    para facilitar o consumo e exploração da api

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import cron from 'node-cron';
 import { createConnection, getConnectionOptions } from 'typeorm';
 import express from 'express';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { CountryResolver } from './resolvers/CountryResolver';
@@ -9,7 +10,7 @@ import { parseApiData } from './worker';
 
 (async () => {
   const app = express();
-
+  app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
   const options = await getConnectionOptions(process.env.MODE || 'database');
   await createConnection({ ...options, name: 'default' });
 
@@ -18,7 +19,8 @@ import { parseApiData } from './worker';
       resolvers: [CountryResolver],
       validate: true
     }),
-    context: ({ req, res }) => ({ req, res })
+    context: ({ req, res }) => ({ req, res }),
+    playground: true
   });
 
   apolloServer.applyMiddleware({ app, cors: false });
